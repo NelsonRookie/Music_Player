11f278c015d9144daaa9704fb8dcbb22ac8c0c49
NelsonRookie <real.nelson92@gmail.com>
2024-07-09 21:09:12 +0800

    this update includes the function of playing music

--- scripts.js
+++ scripts.js
@@ -91,7 +91,12 @@ const playSong = (id) => {
   audio.src = song.src;
   audio.title = song.title;
 
-
+  
+  if(userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  }else {
+    audio.currentTime = userData?.songCurrentTime;
+  }
 };
 
 const renderSongs = (array) => {

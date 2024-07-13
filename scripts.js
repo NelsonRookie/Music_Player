952247b9906afa6863c698c35b7f85dd7f4cad66
NelsonRookie <real.nelson92@gmail.com>
2024-07-13 18:06:43 +0800

    adding function to display the title of current playing song

--- scripts.js
+++ scripts.js
@@ -103,6 +103,16 @@ const playSong = (id) => {
 
 
   setPlayerDisplay();
+  setPlayButtonAccessibleText();
+};
+
+const setPlayButtonAccessibleText = () => {
+  const song = userData?.currentSong || userData?.songs[0];
+
+  playButton.setAttribute(
+    "aria-label",
+    song?.title ? `Play ${song.title}` : "Play"
+  );
 };
 
 const pauseSong = () => {
